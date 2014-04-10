a7f2f37c556249e2b50a89fdc8dd27e155d8fdf3
Julia K Szopa <julia.krysztofiak@blackbox.vc>
2014-04-09 23:17:57 -0700

    foo

---
+++ _site/assets/js.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+//Launch carouser on mainfold 
+	$("#carousel-home").owlCarousel({
+		items: 1,
+    	itemsScaleUp: true,
+    	itemsDesktop : [1199,1],
+    	itemsDesktopSmall : [980,1],
+    	itemsTablet: [768,1],
+    	itemsMobile : [479,1],
+    	//Basic Speeds
+	    slideSpeed : 200,
+	    paginationSpeed : 800,
+	    rewindSpeed : 1000,
+	    //Autoplay
+	    autoPlay : false,
+	    stopOnHover : true,
+	  	});
+});
